4193f26d0700cd0ccece52d2512e75c2822e119b
Kastyao <mr.Kastyao@gmail.com>
2016-03-04 18:47:57 +0200

    lesson-6/List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,10 +13,21 @@ namespace Snake
 
 
             Point p1 = new Point(1,3,'*');
-                  p1.Draw();
+                  //p1.Draw();
 
             Point p2 = new Point(4,5,'#');           
-                  p2.Draw();
+                  //p2.Draw();
+
+            List<Point> pointsList = new List<Point>();
+            pointsList.Add( new Point(1, 3, '*'));
+            pointsList.Add( new Point(4, 5, '#'));
+            pointsList.Add( new Point(5, 6, '░'));
+            pointsList.Add( new Point(6, 8, '*'));
+            
+            for(int i=0; i<pointsList.Count; i++)
+            {
+                pointsList[i].Draw();
+            }
 
             Console.ReadLine();
         }
