482bdc0fc15dc45b5db1ac4a043ad85899c4825f
Kastyao <mr.Kastyao@gmail.com>
2016-03-04 19:10:32 +0200

    Урок 7 - Класс "Горизонтальные линии"

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> hLine;
+
+        public HorizontalLine(int leftX, int rightX, int y, char symb)
+        {
+            hLine = new List<Point>();
+            for (int x = leftX; x <= rightX; x++)
+            {
+                Point p = new Point(x, y, symb);
+                hLine.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in hLine)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
